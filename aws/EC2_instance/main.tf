8205199c32138e4f8282fc0483face744b161d9f
arsalan.ahmed <arsalanabc@gmail.com>
2019-06-25 12:07:40 +0100

    launch simple ec2 instance in eu-west-1

--- aws/EC2_instance/main.tf
+++ aws/EC2_instance/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "instance" {
-  ami = "ami-0e55e373"
+resource "aws_instance" "test" {
+  ami = "ami-ee0b0688"
   instance_type = "t1.micro"
   tags {
     Name = "test-ec2"
