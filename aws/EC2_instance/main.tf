48160451a9656ebb74d75cb46557f564b6f7780e
arsalan.ahmed <arsalanabc@gmail.com>
2019-06-25 16:42:56 +0100

    add security group to allow ingress on 8080

--- aws/EC2_instance/main.tf
+++ aws/EC2_instance/main.tf
@@ -5,12 +5,23 @@ provider "aws" {
 resource "aws_instance" "test" {
   ami = "ami-ee0b0688"
   instance_type = "t1.micro"
+  security_groups = ["${aws_security_group.instance.name}"]
   tags {
     Name = "test-ec2"
   }
 
   user_data = <<-EOF
     echo "hello world" > index.html
-    nohup busybox httpd -fp 8080 &
+    nohup busybox httpd -f -p 8080 &
     EOF
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
