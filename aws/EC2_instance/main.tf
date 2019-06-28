1f7a7481c8c1b7a88ffc8e18110ce239b8f2e378
arsalan.ahmed <arsalanabc@gmail.com>
2019-06-28 11:36:08 +0100

    deploy a cluster of three web servers

--- aws/EC2_instance/main.tf
+++ aws/EC2_instance/main.tf
@@ -7,17 +7,47 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "test-ec2-1" {
   ami = "ami-ee0b0688"
   instance_type   = "t1.micro"
   security_groups = ["${aws_security_group.instance.name}"]
   tags {
-    Name = "test-ec2"
+    Name = "test-ec2-1"
   }
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello from instance 1" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+}
+
+resource "aws_instance" "test-ec2-2" {
+  ami = "ami-ee0b0688"
+  instance_type   = "t1.micro"
+  security_groups = ["${aws_security_group.instance.name}"]
+  tags {
+    Name = "test-ec2-2"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello from instance 2" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+}
+
+resource "aws_instance" "test-ec2-3" {
+  ami = "ami-ee0b0688"
+  instance_type   = "t1.micro"
+  security_groups = ["${aws_security_group.instance.name}"]
+  tags {
+    Name = "test-ec2-3"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello from instance 3" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 }
