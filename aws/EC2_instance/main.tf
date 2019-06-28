844e485a55203ed5a33761c33a81ff9306a411d1
arsalan.ahmed <arsalanabc@gmail.com>
2019-06-28 10:49:42 +0100

    extact port to a variable

--- aws/EC2_instance/main.tf
+++ aws/EC2_instance/main.tf
@@ -1,3 +1,8 @@
+variable "server_port" {
+  description = "Port to allow income/outgoing requests"
+  default     = "8080"
+}
+
 provider "aws" {
   region = "eu-west-1"
 }
@@ -13,15 +18,15 @@ resource "aws_instance" "test" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "terraform-example-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
